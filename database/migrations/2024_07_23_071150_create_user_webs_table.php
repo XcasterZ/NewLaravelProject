f3c6f88e9a633b423b4d4f9fe9cc9bf60dad1e36
XcasterZ <xct.xcaster@gmail.com>
2024-08-03 22:55:13 +0700

    first commit

---
+++ database/migrations/2024_07_23_071150_create_user_webs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserWebsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('user_webs', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->text('profile_detail')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('line')->nullable();
+            $table->string('profile_img')->default('Profile Pic/default.png');
+            $table->json('rating')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('user_webs');
+    }
+}
