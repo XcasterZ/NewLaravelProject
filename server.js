0ec0540b66e3792b1dcc2bd3e432ed466ae5c3ac
XcasterZ <xct.xcaster@gmail.com>
2024-08-04 00:15:56 +0700

    Update configuration

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8080; // ใช้ PORT จาก environment variables หรือ 8080 เป็นค่าเริ่มต้น
+const port = process.env.PORT || 9000; // ใช้ PORT จาก environment variables หรือ 9000 เป็นค่าเริ่มต้น
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
