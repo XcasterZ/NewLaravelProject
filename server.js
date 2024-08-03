4d63e5af5c4cad5317ecbc7bcea8ced7817b7c83
XcasterZ <xct.xcaster@gmail.com>
2024-08-04 00:09:35 +0700

    Update server to use PORT environment variable

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8080; // ใช้ PORT จาก environment variables หรือ 8080 เป็นค่าเริ่มต้น
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://0.0.0.0:${port}`);
 });
