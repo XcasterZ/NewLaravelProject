f80ac5b7670feae8ed64da7e0a600cfc1ba7622b
XcasterZ <xct.xcaster@gmail.com>
2024-08-03 23:44:51 +0700

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:${port}`);
 });
