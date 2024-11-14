32c94cc26bc086b83dcd36ab65312977ffa534bc
Pornsawan Praman <xct.xcaster@gmail.com>
2024-11-14 14:07:06 +0700

    close_btn

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -165,8 +165,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="forgotPasswordModalLabel">Reset Password</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span >&times;</span>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="&times;">
                     </button>
                 </div>
                 <div class="modal-body">
