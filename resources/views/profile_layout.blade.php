8f94af7f726cb8f1c76580a833579cfd16215d31
Pornsawan Praman <xct.xcaster@gmail.com>
2024-11-21 20:50:29 +0700

    swal-update-profile-img

--- resources/views/profile_layout.blade.php
+++ resources/views/profile_layout.blade.php
@@ -5,23 +5,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('css/product_card.css')}}">
-    <link rel="stylesheet" href="{{asset('css/profle.css')}}">
-    <link rel="stylesheet" href="{{asset('css/mobile_filter.css')}}">
-    <link rel="stylesheet" href="{{asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/cart.css')}}">
-    <script src="{{asset('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js')}}"></script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/product_card.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/profle.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/mobile_filter.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
+    <script src="{{ asset('https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js') }}"></script>
     {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> --}}
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <style>
         body {
             font-family: 'Kanit', sans-serif;
         }
     </style>
-
-    
     <style>
         .file-info {
             font-size: 14px;
@@ -29,11 +29,14 @@
             margin-top: 10px;
         }
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 
 </head>
 
 <body>
-    
+
     <section id="header">
         <div class="logo">
             <h4 style="color: white; letter-spacing: 2px;">สะดวกพระ</h4>
@@ -48,8 +51,8 @@
                     <li><a href="{{ route('auth.register') }}">สมัครสมาชิก</a></li>
                     <li><a href="{{ route('auth.login') }}">เข้าสู่ระบบ</a></li>
                 @else
-                    <li><a class="cart_profile" href="{{ route('cart.show') }}"><img src="{{asset('Component Pic/Cart.png')}}" width="30"
-                                height="30"></a></li>
+                    <li><a class="cart_profile" href="{{ route('cart.show') }}"><img
+                                src="{{ asset('Component Pic/Cart.png') }}" width="30" height="30"></a></li>
                     <li><a class="cart_profile" href="{{ route('profile.edit') }}"><img
                                 src="{{ asset(Auth::user()->profile_img ?? 'Profile Pic/default.png') }}" width="30"
                                 height="30" style="border-radius: 50%; object-fit: cover;"></a></li>
@@ -64,7 +67,7 @@
         </div>
         <div id="navbar2">
             <div class="close_menu">
-                <button id="closeMenuButton" ><i  class="fa fa-times"></i></button>
+                <button id="closeMenuButton"><i class="fa fa-times"></i></button>
             </div>
             @auth
                 <div class="profile_mobile">
@@ -93,7 +96,7 @@
                 <div class="box_menu">
                     <a href="{{ route('cart.show') }}"><button>ตะกร้า</button></a>
                 </div>
-                
+
             @endguest
         </div>
 
@@ -118,7 +121,7 @@
 
     <section class="container">
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
         <div class="grid">
             <div class="profile_content1">
@@ -146,7 +149,8 @@
                             <span class="star-rating">
                                 @for ($i = 1; $i <= 5; $i++)
                                     <label for="rate-{{ $i }}" style="--i:{{ $i }}">
-                                        <i class="fa-solid fa-star" style="color: {{ $i <= $averageRating ? '#faec1b' : '#d3d3d3' }}; font-size: 24px;"></i>
+                                        <i class="fa-solid fa-star"
+                                            style="color: {{ $i <= $averageRating ? '#faec1b' : '#d3d3d3' }}; font-size: 24px;"></i>
                                     </label>
                                 @endfor
                             </span>
@@ -165,25 +169,25 @@
         <div class="contact">
             <p>PORNSAWAN PRAMAN</p>
             <ul>
-                <li><a href=""><img src="{{asset('Component Pic/facebook.webp')}}" alt=""></a></li>
-                <li><a href=""><img src="{{asset('Component Pic/ig.webp')}}" alt=""></a></li>
-                <li><a href=""><img src="{{asset('Component Pic/email.png')}}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/facebook.webp') }}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/ig.webp') }}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/email.png') }}" alt=""></a></li>
             </ul>
         </div>
         <div class="contact">
             <p>PATTARADON WONGCHAI</p>
             <ul>
-                <li><a href=""><img src="{{asset('Component Pic/facebook.webp')}}" alt=""></a></li>
-                <li><a href=""><img src="{{asset('Component Pic/ig.webp')}}" alt=""></a></li>
-                <li><a href=""><img src="{{asset('Component Pic/email.png')}}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/facebook.webp') }}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/ig.webp') }}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/email.png') }}" alt=""></a></li>
             </ul>
         </div>
         <div class="contact">
             <p>LAMPANG RAJABHAT UNIVERSITY</p>
             <ul>
-                <li><a href=""><img src="{{asset('Component Pic/facebook.webp')}}" alt=""></a></li>
-                <li><a href=""><img src="{{asset('Component Pic/www.png')}}" alt=""></a></li>
-                <li><a href=""><img src="{{asset('Component Pic/email.png')}}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/facebook.webp') }}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/www.png') }}" alt=""></a></li>
+                <li><a href=""><img src="{{ asset('Component Pic/email.png') }}" alt=""></a></li>
             </ul>
         </div>
     </section>
@@ -207,17 +211,26 @@
                 .then(response => response.json())
                 .then(data => {
                     if (data.success) {
-                        alert(data.success);
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'สำเร็จ!',
+                            text: 'อัพเดทรูปโปรไฟล์เรียบร้อยแล้ว',
+                            confirmButtonText: 'ตกลง'
+                        });
                         if (data.profile_img) {
                             output.src = '{{ asset('') }}' + data.profile_img;
                         }
                     } else {
-                        alert('Error uploading profile image');
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'เกิดข้อผิดพลาด!',
+                            text: 'ไม่สามารถอัพโหลดรูปโปรไฟล์ได้',
+                            confirmButtonText: 'ตกลง'
+                        });
                     }
                 })
                 .catch(error => {
                     console.error(error);
-                    alert('Error uploading profile image');
                 });
         }
     </script>
@@ -256,7 +269,7 @@
     <script src="/js/script.js"></script>
     <script src="/js/script2.js"></script>
     <script src="https://kit.fontawesome.com/d671ca6a52.js" crossorigin="anonymous"></script>
-    
+
 </body>
 
 </html>
